3a83976f000571f896b48d51986d3cf826cc7476
KummariSaiteja <saiteja@lyrostech.com>
2024-04-15 22:57:25 +0530

    initial commit

---
+++ src/Context/UserProvider.js
@@ -0,0 +1,16 @@
+import React,{useState} from "react";
+import UserContext from "./UserContext";
+
+const UserProvider=(props)=>{
+    const [token,setToken]=useState("");
+    return(
+        <UserContext.Provider value={{
+            token:token,
+            setToken:setToken
+        }}>
+            {props.childern}
+        </UserContext.Provider>
+    )
+}
+
+export default UserProvider;
